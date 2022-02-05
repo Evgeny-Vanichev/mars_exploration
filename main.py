fc796cc0d72dd1f60326f9814a5d1b494b7248b9
evgeny <e.vanitschev@yandex.ru>
2022-02-05 21:50:56 +0300

    Второе задание из ДЗ

--- main.py
+++ main.py
@@ -259,6 +259,30 @@ def choice(name):
                               </body>
                             </html>"""
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f"""<!doctype html>
+                                <html lang="en">
+                                  <head>
+                                    <meta charset="utf-8">
+                                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                                    <link rel="stylesheet" 
+                                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                                    crossorigin="anonymous">
+                                    <link rel="stylesheet" href="/static/css/style.css">
+                                    <title>Привет, Марс!</title>
+                                  </head>
+                                  <body>
+                                    <h1 class=surprise>Результаты отбора</h1>
+                                    <h2>Претендента на участие в миссии {nickname}</h2>
+                                    <div class="alert alert-success">Поздравляем! Ваш рейтинг после {level} этапа отбора</div>
+                                    <h2>составляет {rating}</h2>
+                                    <div class="alert alert-warning">Желаем удачи!</div>
+                                    <button onclick="window.location.href = '/';" color="#ffcc00">На главную</button>
+                                  </body>
+                                </html>"""
+
 
 @app.route('/')
 def start_page():
